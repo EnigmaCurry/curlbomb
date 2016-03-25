99b474c67a71e180cb25f7b07764688a959a30c5
Ryan McGuire <ryan@enigmacurry.com>
2016-03-24 20:12:25 -0400

    Release 1.0.1 which includes correct description and license

---
+++ register.py
@@ -0,0 +1,13 @@
+import pypandoc
+import os
+
+with open('README.txt','w+') as rst_file:
+    with open('README.md') as markdown_file:
+        # Read the first line and throw it away, as it's the same description as in setup.py
+        markdown_file.readline()
+        readme = markdown_file.read()
+    
+    rst_file.write(pypandoc.convert(readme,'rst', format='markdown'))
+
+os.system("python setup.py register")
+os.remove('README.txt')
--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
+long_description = description = "A personal HTTP server for serving one-time-use bash scripts"
+if os.path.exists('README.txt'):
+    long_description=open('README.txt').read()
 
 setup(name='curlbomb',
-      version='1.0',
-      description='A personal HTTP server for serving one-time-use bash scripts',
+      version='1.0.1',
+      description=description,
+      long_description=long_description,
+      license="MIT",
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+      ],
       author='Ryan McGuire',
       author_email='ryan@enigmacurry.com',
       url='https://github.com/EnigmaCurry/curlbomb',
