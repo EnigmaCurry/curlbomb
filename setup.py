fec57cf92e936fd45793822c200b07718e1739e5
Ryan McGuire <ryan@enigmacurry.com>
2016-05-05 15:08:36 -0400

    Bump version to 1.4.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if os.path.exists('README.txt'):
         long_description=f.read()
 
 setup(name='curlbomb',
-      version='1.4.0',
+      version='1.4.1',
       description=description,
       long_description=long_description,
       license="MIT",
